2f2cc8853524f80bbbcf11dc65e2aff6308d7c1a
templar-ajay <templar.ajay@gmail.com>
2024-04-16 01:56:59 +0530

    add error handler serve public files, create corsOptions, request and error logger

--- server.js
+++ server.js
@@ -1,50 +1,103 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 3500;
+const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const { errorHandler } = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// custom middleware: log requests
+app.use(logger);
+
+// third party middleware cors for allowing cross origin resource sharing
+const whitelist = [
+  "https://www.google.com",
+  "http://localhost:3000",
+  "http://127.0.0.1:5500",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // allow origin as undefined in development environment
+    if (whitelist.includes(origin) || (process.env.ENV === "dev" && !origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("origin blocked, not allowed by CORS"));
+    }
+    console.log("origin", origin);
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// built in middleware to handle urlencoded data
+// in other words, form data
+// 'content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// built in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("^/$|/index(.htm(l)?)?", (req, res, next) => {
+  //res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+app.get("/new-page(.htm(l)?)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
+app.get("/old-page(.htm(l)?)?", (req, res) => {
+  res.redirect(301, "/new-page.html"); //302 by default
 });
 
 // Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
-
+app.get(
+  "/hello(.htm(l)?)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
 
 // chaining route handlers
 const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
+  console.log("one");
+  next();
+};
 
 const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
+  console.log("two");
+  next();
+};
 
 const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
+  console.log("three");
+  res.send("Finished!");
+};
 
-app.get('/chain(.html)?', [one, two, three]);
+app.get("/chain(.htm(l)?)?", [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({
+      error: "404 not found",
+    });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
