65d56bc522659d0a5ea7fee18bdb68ab40863dc8
templar-ajay <templar.ajay@gmail.com>
2024-04-21 03:23:48 +0530

    add 3 types of users

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -7,17 +7,26 @@ const verifyRoles = require("../../middleware/verifyRoles");
 router
   .route("/")
   .get(
-    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.User),
+    verifyRoles(ROLES_LIST.SuperAdmin, ROLES_LIST.Admin, ROLES_LIST.User),
     employeesController.getAllEmployees
   )
-  .post(verifyRoles(ROLES_LIST.Admin), employeesController.createNewEmployee)
-  .put(verifyRoles(ROLES_LIST.Admin), employeesController.updateEmployee)
-  .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
+  .post(
+    verifyRoles(ROLES_LIST.SuperAdmin, ROLES_LIST.Admin),
+    employeesController.createNewEmployee
+  )
+  .put(
+    verifyRoles(ROLES_LIST.SuperAdmin, ROLES_LIST.Admin),
+    employeesController.updateEmployee
+  )
+  .delete(
+    verifyRoles(ROLES_LIST.SuperAdmin, ROLES_LIST.Admin),
+    employeesController.deleteEmployee
+  );
 
 router
   .route("/:id")
   .get(
-    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.User),
+    verifyRoles(ROLES_LIST.SuperAdmin, ROLES_LIST.Admin, ROLES_LIST.User),
     employeesController.getEmployee
   );
 
