60d9e2d3fe24a2889cc4f54e3e3abca796ea75d8
templar-ajay <templar.ajay@gmail.com>
2024-04-23 17:50:19 +0530

    moved a few things

--- config/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:3000",
-  "http://127.0.0.1:5500",
-];
-
-module.exports = allowedOrigins;
--- config/roles_list.js
+++
@@ -1,7 +0,0 @@
-const ROLES_LIST = {
-  SuperAdmin: 1420,
-  Admin: 5150,
-  User: 2001,
-};
-
-module.exports = ROLES_LIST;
