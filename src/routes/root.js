57cf52d5cee4a95052f1a7ffc98ff6e842f0d38f
templar-ajay <templar.ajay@gmail.com>
2024-04-24 20:45:20 +0530

    update: send json response

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 
 router.get("^/$|^/index(.htm(l)?)?", (req, res, next) => {
-  //res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+  res.json({ message: "hello from scale api" });
 });
 
 module.exports = router;
