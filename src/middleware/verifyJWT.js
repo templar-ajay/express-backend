9c900fb8c3228e4a847015b0621be9d4da01bb56
templar-ajay <templar.ajay@gmail.com>
2024-04-25 19:41:58 +0530

    refresh access token if it's invalid

--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -1,14 +1,16 @@
 const jwt = require("jsonwebtoken");
+const { handleRefreshToken } = require("./refreshToken");
 require("dotenv").config();
 
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+  const accessToken = req.cookies["access-token"];
+  if (!accessToken) return res.sendStatus(401);
 
-  const token = authHeader.split(" ")[1];
+  jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      handleRefreshToken(req, res, next);
+    }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); // invalid token
     req.user = decoded.UserInfo.username;
     req.role = decoded.UserInfo.role;
     next();
